0a14601caa278689d5d3cc3129b89ba42e27ebf7
Railene <rbds.ecp19@uea.edu.br>
2022-08-29 12:59:49 -0400

    primeiro commit

---
+++ src/util/ConversorMoeda.java
@@ -0,0 +1,8 @@
+package util;
+
+public class ConversorMoeda {
+	public static double IOF = 0.06;
+    public static double dollarParaReal(double quantia, double precoDollar) {
+    return quantia * precoDollar * (1.0 + IOF);
+  }
+}
